05014e5feabc603a5e841f5b1fbcbca5444e39e7
ahmad <ayashizade@gmail.com>
2020-03-20 23:29:44 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  /*  $(window).scroll(function(){
+        if($(this).scroll()>100 ){
+            $('.navbar').addClass('css2');
+        } else {
+            $('#navbar').remove('css2');
+        }
+    });*/
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
